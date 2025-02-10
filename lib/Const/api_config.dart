6904ae17f4f71f72a7fc90280fb339aad677992a
mdtaiyabhesabe <106661707+mdtaiyabhesabe@users.noreply.github.com>
2025-02-10 10:55:21 +0300

    B4Pos

---
+++ lib/Const/api_config.dart
@@ -0,0 +1,8 @@
+class APIConfig {
+  // static String domain = 'https://pospro.acnoo.com/';
+  static String domain = 'https://pospro.acnoo.com/';
+  static String url = '${domain}api/v1';
+  static String registerUrl = '/sign-up';
+  static String businessCategoriesUrl = '/business-categories';
+  static String businessSettingsUrl = '/business-settings';
+}
